a820198e790714e82b5983e98c88c809df8d7296
ABDUL RAHAMAN <36rahaman@gmail.com>
2024-02-21 14:54:06 +0530

    chore: Files added from js to tsx

---
+++ src/views/HomePageView.tsx
@@ -0,0 +1,10 @@
+import Home_V6 from '@/app/(home)/home-v6/page';
+import Wrapper from '@/app/layout-wrapper/wrapper';
+
+export default function HomePage() {
+  return (
+    <Wrapper>
+      <Home_V6 />
+    </Wrapper>
+  );
+}
