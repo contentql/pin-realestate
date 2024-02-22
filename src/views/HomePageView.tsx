e5d9df4c865d7f16e5b58fc589d6475066961e73
Rahaman <115566846+Rahaman002@users.noreply.github.com>
2024-02-22 11:22:14 +0530

    Ar client homepage (#2)
    * chore: Files added from js to tsx
    
    * chore: "scss added"
    
    * chore: removed dropdown for home and set default route to home
    
    * chore: images update for homepage
    
    * chore: Added properties and page not found route
    
    * chore: images updated
    
    * chore: Added not found page
    
    * chore: added properties route
    
    * chore: Update folders
    
    * chore: update route for featured properties
    
    * chore: folders renamed
    
    ---------
    
    Co-authored-by: charanm927 <charanm927@gmail.com>

---
+++ src/views/HomePageView.tsx
@@ -0,0 +1,10 @@
+import Home_V6 from '@/app/(nav)/(home)/home/page';
+import Wrapper from '@/app/layout-wrapper/wrapper';
+
+export default function HomePage() {
+  return (
+    <Wrapper>
+      <Home_V6 />
+    </Wrapper>
+  );
+}
