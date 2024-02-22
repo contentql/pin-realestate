015a12fe8cb5f4e3bf76ec88863f6bd61e7fe1ac
Rahaman <115566846+Rahaman002@users.noreply.github.com>
2024-02-22 12:58:15 +0530

    Ar individual property (#4)
    * chore: added individual product route
    
    * chore: navbar folder updated

--- src/components/home/home-v6/FeatuerdListings.tsx
+++ src/components/home/home-v6/FeatuerdListings.tsx
@@ -1,10 +1,11 @@
 'use client';
-import listings from '@/data/listings';
 import Image from 'next/image';
 import Link from 'next/link';
+import React from 'react';
 import { Navigation, Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.min.css';
+import listings from '../../../data/listings';
 
 const FeaturedListings = () => {
   return (
@@ -37,7 +38,7 @@ const FeaturedListings = () => {
           },
         }}
       >
-        {listings.slice(0, 5).map((listing: any) => (
+        {listings.slice(0, 5).map((listing) => (
           <SwiperSlide key={listing.id}>
             <div className='item'>
               <div className='listing-style1'>
@@ -63,9 +64,7 @@ const FeaturedListings = () => {
                 </div>
                 <div className='list-content'>
                   <h6 className='list-title'>
-                    <Link href={`/property/${listing.id}`}>
-                      {listing.title}
-                    </Link>
+                    <Link href='/properties'>{listing.title}</Link>
                   </h6>
                   <p className='list-text'>{listing.location}</p>
                   <div className='list-meta d-flex align-items-center'>
--- src/views/HomePageView.tsx
+++ src/views/HomePageView.tsx
@@ -1,5 +1,7 @@
-import Home from "@/app/(nav)/(home)/home/page";
-import Wrapper from "@/app/layout-wrapper/wrapper";
+
+import Home from '@/app/(nav)/home/page';
+import Wrapper from '@/app/layout-wrapper/wrapper';
+
 
 export default function HomePage() {
   return (
