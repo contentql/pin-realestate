085a47be3c5d0f2c2eba0bc74c49d3489b511965
charanm927 <charanm927@gmail.com>
2024-02-22 12:31:29 +0530

    chore: contact and about pages are added and blogs removed

--- src/views/HomePageView.tsx
+++ src/views/HomePageView.tsx
@@ -1,10 +1,10 @@
-import Home_V6 from '@/app/(nav)/(home)/home/page';
-import Wrapper from '@/app/layout-wrapper/wrapper';
+import Home from "@/app/(nav)/(home)/home/page";
+import Wrapper from "@/app/layout-wrapper/wrapper";
 
 export default function HomePage() {
   return (
     <Wrapper>
-      <Home_V6 />
+      <Home />
     </Wrapper>
   );
 }
