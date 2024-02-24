f6ff3ee93eb23e676f76b53093bc3eb4a1d1f574
julydeath <manojkarajada.mk@gmail.com>
2024-02-25 02:13:00 +0530

    fix : conflict and trpc route type error

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,11 +1,13 @@
-import { appRouter } from '@/trpc';
-import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
+import { appRouter } from "@/trpc";
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 
 const handler = (req: Request) => {
   fetchRequestHandler({
-    endpoint: '/api/trpc',
+    endpoint: "/api/trpc",
     req,
     router: appRouter,
+    // context already passed from express middleware
+    // @ts-expect-error
 
     createContext: () => ({}),
   });
