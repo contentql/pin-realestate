d6771e11dff659a698a267e0ad0936e846f37c23
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2024-02-25 16:29:10 +0530

    chore: updated remotePatterns for images

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'payload-in-next-production.up.railway.app', //TODO: update accordingly
+        hostname: 'pin-realestate-production.up.railway.app',
       },
     ],
   },
